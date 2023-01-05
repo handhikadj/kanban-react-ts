1a060ec1b04054a694927f0d5610d1531c0a6b20
Handika Dwi <githubhandhikadj@gmail.com>
2023-01-05 16:30:50 +0700

    setup react-query for server-state management and configure typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from '@/App'
 import '@/styles/app.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        }
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+
+            {import.meta.env.VITE_APP_ENV !== 'production' ? <ReactQueryDevtools /> : null}
+        </QueryClientProvider>
     </React.StrictMode>,
 )
